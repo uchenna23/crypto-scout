fd24c1f735267be6a22f87ff02c0f55a0ec51cd9
uchenna23 <uchennanwagbara23@gmail.com>
2025-02-18 00:38:19 -0500

    Reverting changes

--- src/main/java/com/example/crypto_scout/CryptoScoutApplication.java
+++ src/main/java/com/example/crypto_scout/CryptoScoutApplication.java
@@ -3,19 +3,13 @@ package com.example.crypto_scout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import java.util.Collections;
 
 @SpringBootApplication
 @EnableCaching
 public class CryptoScoutApplication {
 
-    public static void main(String[] args) {
-        String portEnv = System.getenv("PORT");
-        System.out.println("Environment variable PORT: " + portEnv);
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoScoutApplication.class, args);
+	}
 
-        // Force the server port using a system property
-        SpringApplication app = new SpringApplication(CryptoScoutApplication.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "80"));
-        app.run(args);
-    }
 }
